f83004c374f2018e703f9d40496660c9dd829d59
anohige <dmanirudh16nov@gmail.com>
2025-03-20 13:04:12 +0100

    modified modality_stats.py

--- stats/modality_stats.py
+++ stats/modality_stats.py
@@ -8,18 +8,23 @@ class Modality_stats:
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         self.device = adafruit_dht.DHT11(board.D4)
-        self.fetch_stats()
 
     def fetch_stats(self):
-        try:
-            while True:
-                self.temperature = self.device.temperature()
-                self.humidity = self.device.humidity()
-                print(F"Temperature: {self.temperature} C Humidity: {self.humidity}%")
-                time.sleep(1)
-        except RuntimeError as e:
-            print(e)
+        # try:
+        #     while True:
+        #         self.temperature = self.device.temperature()
+        #         self.humidity = self.device.humidity()
+        #         print(F"Temperature: {self.temperature} C Humidity: {self.humidity}%")
+        #         time.sleep(1)
+        # except RuntimeError as e:
+        #     print(e)
+        #
+        while True:
+            try:
+                temperature = self.device.temperature()
+                humidity = self.device.humidity()
+                print(F"Temperature: {temperature} C Humidity: {humidity}%")
+            except RuntimeError as e:
+                print(e)
 
-
-
-
+            time.sleep(1)
